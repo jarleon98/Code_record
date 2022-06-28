149fc820d748f4bd675eb7bb045971e77e9c75c9
animuss <964736787@qq.com>
2022-06-28 21:37:57 +0800

    力扣104

---
+++ leetcode_handWriting/104二叉树最大深度.js
@@ -0,0 +1,9 @@
+var maxDepth = function(root) {
+    if(!root) {
+        return 0
+    } else {
+        let left = maxDepth(root.left);
+        let right = maxDepth(root.right);
+        return Math.max(left, right) + 1
+    }
+};
